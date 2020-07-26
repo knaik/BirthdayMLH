c1842ab39ce8bfc09e7c61795cfb07acc3745ebc
knaik <kn172@rutgers.edu>
2020-07-26 00:27:53 -0400

    self motivational learn by failing and doing better next time

--- runcmd.sh
+++ runcmd.sh
@@ -8,3 +8,5 @@ flask run --host=0.0.0.0 & ~/ngr/ngrok http 5000
 #sudo netstat -tulnp | grep :5000
 #sudo kill 32060
 
+#I know this is a terrible way to do this, I'm better off separating ngrok and the flask so I can export and run and I 
+#know things are redundant, but that's how I will learn
